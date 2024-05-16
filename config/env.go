e2551086b3cfcdde952c94f1f8e596fd9fa78bce
jiayishen21 <lucasshen21@gmail.com>
2024-05-16 15:00:35 -0400

    init project

---
+++ config/env.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	PublicHost string
+	Port       string
+	DBUser     string
+	DBPassword string
+	DBAddress  string
+	DBName     string
+}
+
+// create global variable so that env isn't reinitialized every time it's called
+var Envs = initConfig()
+
+func initConfig() Config {
+	godotenv.Load()
+
+	return Config{
+		PublicHost: getEnv("PUBLIC_HOST", "http://localhost"),
+		Port:       getEnv("PORT", "8080"),
+		DBUser:     getEnv("DB_USER", "root"),
+		DBPassword: getEnv("DB_PASSWORD", "Password1"),
+		DBAddress:  fmt.Sprintf("%s:%s", getEnv("DB_ADDRESS", "localhost"), getEnv("DB_PORT", "3306")),
+		DBName:     getEnv("DB_NAME", "time_capsule"),
+	}
+}
+
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
